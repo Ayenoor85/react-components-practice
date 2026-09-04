df036504d420673925ad4f6a2d17cbf38ad7c75b
Ayenoor85 <ayenoor85@gmail.com>
2026-09-04 11:21:43 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Accordian from "./components/accordion";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Accordian />
+       <RandomColor />
+       <StarRating/>
+       <ImageSlider
+       url="https://picsum.photos/v2/list"
+      page={1}
+      limit={10}
+       />
+      <LoadMoreData/>
+      <TreeView menus={menus}/>
+    </div>
+  );
+}
+
+export default App;
